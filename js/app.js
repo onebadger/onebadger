9951d5cd098f93ab4b0badc5c87c5cf3078478d6
onebadger <yakov.barsukov@gmail.com>
2017-07-18 10:07:25 +0300

    files update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// dropdown button
+// nav dropdown button
 
 function dropdownClick() {
     document.getElementById("dropdown-menu").classList.toggle("show");
@@ -13,6 +13,26 @@ window.onclick = function(event) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
+
+// cart dropdown button
+
+function cartClick() {
+    document.getElementById("dropdown-cart").classList.toggle("show2");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.cart-btn')) {
+    
+    var dropdowns = document.getElementsByClassName("cart-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show2')) {
+        openDropdown.classList.remove('show2');
       }
     }
   }
