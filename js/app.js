3ac04c8d8c7513c50e3fbad2e346a8d786d625b0
onebadger <yakov.barsukov@gmail.com>
2017-07-19 21:07:41 +0300

    fix nav-dropdown & cart-dropdown onclick outside the buttons

--- js/app.js
+++ js/app.js
@@ -4,35 +4,57 @@ function dropdownClick() {
     document.getElementById("dropdown-menu").classList.toggle("show");
 }
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropdown-btn')) {
+// cart dropdown button
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
+function cartClick() {
+    document.getElementById("dropdown-cart").classList.toggle("show");
+}
+
+// close nav-dropdown & cart-dropdown onclick outside the buttons
+
+window.onclick = function(event) {
+  
+  if (!event.target.matches('.svg')) {
+    
+    var dropdownContent = document.getElementsByClassName("dropdown-content");
+    var cartContent = document.getElementsByClassName("cart-content");
+    
     var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
+    for (i = 0; i < dropdownContent.length; i++) {
+      var openDropdown = dropdownContent[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
     }
-  }
-};
-
-// cart dropdown button
-
-function cartClick() {
-    document.getElementById("dropdown-cart").classList.toggle("show2");
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches('.cart-btn')) {
+    var j;
+    for (i = 0; i < cartContent.length; i++) {
+      var openDropdowns = cartContent[i];
+      if (openDropdowns.classList.contains('show')) {
+        openDropdowns.classList.remove('show');
+      }
+    }
     
-    var dropdowns = document.getElementsByClassName("cart-content");
+  } else if (!event.target.matches('.dropdown-btn')) {
+    
+    var dropdownContent = document.getElementsByClassName("dropdown-content");
+    
     var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show2')) {
-        openDropdown.classList.remove('show2');
+    for (i = 0; i < dropdownContent.length; i++) {
+      var openDropdown = dropdownContent[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+    
+  } else if (!event.target.matches('.cart-btn')) {
+        
+    var cartContent = document.getElementsByClassName("cart-content");
+    
+    var j;
+    for (i = 0; i < cartContent.length; i++) {
+      var openDropdowns = cartContent[i];
+      if (openDropdowns.classList.contains('show')) {
+        openDropdowns.classList.remove('show');
       }
     }
   }
